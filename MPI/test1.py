22d3d510970d375a0870fd98ccc02b89f0c10936
Maria Clara Sanchez <msanch60@hdpmaster.dis.eafit.edu.co>
2019-03-03 21:30:34 -0500

    arreglando el codigo

--- MPI/test1.py
+++ MPI/test1.py
@@ -1,4 +1,3 @@
-#-*-coding:utf-8-*-
 from mpi4py import MPI
 from csv import DictReader, field_size_limit
 from os import listdir
@@ -11,41 +10,43 @@ arr = []
 arr3 = []
 comm = MPI.COMM_WORLD
 count = 0
+
 def test():
-    if comm.rank == 0:
-        try:
-                    a = input('Ingrese la palabra que desea buscar: ')
-        except ValueError:
-                    print ("Not a string")
-        
-    with open ('/home/jfonsec1/tests/Tests/dc-wikia-data.csv') as csvf:
+    with open ('/home/msanch60/Tests/dc-wikia-data.csv') as csvf:
         readCSV = DictReader(csvf)
         for row in readCSV:
             arr.append(row)
         arr2 = np.array(arr)
         out = np.array_split(arr2,comm.size)
         if comm.rank == 0:
+            try:
+                a = input('Ingrese la palabra que desea buscar: ')
+            except ValueError:
+                print ("Not a string")
             data = out
         else:
             data = None
+            a = None
+                        
+        a = comm.bcast(a, root=0)
         data = comm.scatter(data, root=0)
-        #print ('rank',comm.rank,'has data:',data)
-        cont = 0
-        #arr3 = []
+                
+        #print ('rank',comm.rank,'has data:',a)
+        #print(len(data))
         for row in data:
-           # print(row['name'],cont)
             tempCont = row['name'].lower()
             #tempTitle = row['title'].lower()
             count = tempCont.count(a.lower())
             if(count > 0):
-                tempArr = [count, row['ID'],row['name']
-               # arr3.append(tempArr)
-
-          # print("------------------------------------------------------------------------------------")
-        print("------------------------------------------------------------------------------------")
+                tempArr = [count, row['ID'], row['name']]
+                arr3.append(tempArr)
+            
+        print("en el rango", comm.rank, "el arreglo tiene", len(arr3))
         newData = comm.gather(arr3, root=0)
         if(comm.rank == 0):
-            print (len(newData))
+            newData = [x for x in newData if x != []] 
+            #print (len(newData))
+            print (newData)
         csvf.close()
 
 while decrement:
