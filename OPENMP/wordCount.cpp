c821a5351ac945759020cf04c26c0cf45dbd7e77
Tomas Alvarez <talvare1@hdpmaster.dis.eafit.edu.co>
2019-03-13 17:29:40 -0500

    WIIIIIII no sirve

--- OPENMP/wordCount.cpp
+++ OPENMP/wordCount.cpp
@@ -28,8 +28,8 @@ static void contar(string archivo, vector<vector<string>> csv){
     vector<string> id;
     vector<string> title;
     vector<string> content;
-    vector<int> contador;
-    vector<int> copia;
+    // vector<int> contador;
+    vector<int> copia,copia2;
     ifstream archivoPy(archivo);
     while(getline(archivoPy, linea,'\'')){
         input.push_back(linea);
@@ -53,10 +53,12 @@ static void contar(string archivo, vector<vector<string>> csv){
     cout << "Ingrese la palabra que desee buscar:";
     getline(cin, palabra);
     transform (palabra.begin(), palabra.end(), palabra.begin(), ::tolower);
-#pragma omp parallel private(contador, copia)
+
+ #pragma omp parallel
     {
+  vector<int> contador;
       int cantidadPalabra = 0;
-#pragma omp for schedule(dynamic) 
+#pragma omp for schedule(static)  
       for(int i=0;i<content.size();i++){
 	transform (content.at(i).begin(), content.at(i).end(), content.at(i).begin(), ::tolower);
 	transform (title.at(i).begin(), title.at(i).end(), title.at(i).begin(), ::tolower);
@@ -66,11 +68,19 @@ static void contar(string archivo, vector<vector<string>> csv){
       //cout << contador.size() << endl;
       //}
       //cout << contador.size() << endl;
-      copia = contador;
+#pragma omp critical
+{
+      //copia = contador;
+      copia.insert(copia.end(), make_move_iterator(contador.begin()), make_move_iterator(contador.end()));
+}     //cout << copia.size() << "Holi no joda" << copia.at(0)  << endl;
+     
+      //cout << copia.size() << endl;
+      /*
       sort(contador.begin(), contador.end());
       reverse(contador.begin(), contador.end());
       cout << contador.at(0) << " " << copia.at(0) << endl;
-            
+      */
+  
       /*int i = 0;
       while(i<=9){
 	for(int j=0;j<copia.size();j++){
@@ -80,7 +90,20 @@ static void contar(string archivo, vector<vector<string>> csv){
 	  }
 	}
 	}*/
+    }copia2=copia;
+    sort(copia.begin(),copia.end());
+    reverse(copia.begin(),copia.end());
+    cout << copia.at(0) << endl;
+    int i = 0;
+    while(i<=9){
+      for(int j=0;j<copia2.size();j++){
+	if((copia.at(i)==copia2.at(j)) && (i <= 9)){
+	  cout << copia.at(i) << "/ " << id.at(j) << "/ " << title.at(j) << endl;
+	  i++;
+	}
+      }
     }
+    // cout << copia.size();
 }
 
 int main(int argc, char* argv[]) {
